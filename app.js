bb43b142bb864445341e35f499237908ab3211e0
Raphael Mulenda <mulendaraphael@yahoo.fr>
2025-02-13 13:47:20 +0000

    update v1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello from CICD-Test-App!');
+    res.send('Hello from CICD-Test-App v1!');
 });
 
 const PORT = 80;
